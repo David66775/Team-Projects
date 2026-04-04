1f9df35e7265f3fc0186c6e7115689dc2a1554d7
David66775 <davidjkeesling@gmail.com>
2026-04-03 19:30:21 -0700

    Create Hyper.js

---
+++ assets/Hyper.js
@@ -0,0 +1,28 @@
+document.addEventListener("keydown", function(event) {
+  const key = event.key.toLowerCase();
+  if (event.ctrlKey && key === "u") {
+    event.preventDefault();
+  }
+  if (event.ctrlKey && key === "i") {
+    event.preventDefault();
+  }
+  if (key === "f12") {
+    event.preventDefault();
+  }
+  if (event.ctrlKey && event.shiftKey && key === "j") {
+    event.preventDefault();
+  }
+  if (event.ctrlKey && event.shiftKey && key === "c") {
+    event.preventDefault();
+  }
+  if (event.ctrlKey && event.shiftKey && key === "m") {
+    event.preventDefault();
+  }
+  if (event.key === "Escape") {
+  window.open("", "_self");
+  window.close();
+  }
+  });
+  window.addEventListener('contextmenu', (event) => {
+    event.preventDefault();
+  });
