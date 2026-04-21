ac513c71aedf10444873773211650860df8b194c
David66775 <davidjkeesling@gmail.com>
2026-04-21 11:32:54 -0600

    Update Hyper.js

--- assets/Hyper.js
+++ assets/Hyper.js
@@ -19,7 +19,7 @@ document.addEventListener("keydown", function(event) {
   if (event.ctrlKey && event.shiftKey && key === "m") {
     event.preventDefault();
   }
-  if (event.key === "Escape") {
+  if (event.key === "Escape" || key === "esc") {
   window.open("https://david66775.github.io/Blank", "_self");
   window.close();
   }
